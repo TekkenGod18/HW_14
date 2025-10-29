f22bb4429e7736e6c4b3903d2e03c2489ed7ba1e
Administrator <admin@example.com>
2025-10-30 00:03:22 +0300

    Добавьте файлы проекта.

---
+++ HW_14/Animal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_14
+{
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal()
+        {
+
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название животного: {Name}");
+            Console.Write("Звук: ");
+            Say();
+        }
+    }
+
+    public class Cat : Animal
+    {
+        private string _name;
+
+        public override string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Cat() : base()
+        {
+            Name = "Кот";
+        }
+
+        public Cat(string name) : base()
+        {
+            Name = name;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        private string _name;
+
+        public override string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Dog() : base()
+        {
+            Name = "Собака";
+        }
+
+        public Dog(string name) : base()
+        {
+            Name = name;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ HW_14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка");
+            Dog dog = new Dog("Пёс");
+
+            //cat.ShowInfo();
+
+            dog.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
